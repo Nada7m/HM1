1323cd35c7ba5e6999b3f05ff74ef652d0a4b74a
Nada7m <nadamo123123@gmail.com>
2025-03-18 07:28:01 +0300

    تعديلات
    تعديلات بنقل اكواد الجافا الى ملف مخصص

---
+++ HM3.js
@@ -0,0 +1,10 @@
+function tasbih (){
+var count = parseInt(document.getElementById('num').innerHTML)+1;
+document.getElementById('num').innerHTML = count;
+}
+function Pcolor () {
+document.getElementById('num').style.backgroundColor = "#ffe5cc";
+}
+function resit (){
+document.getElementById('num').innerHTML = 0;
+}

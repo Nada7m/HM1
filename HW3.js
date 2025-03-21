fcfae6a1d6a27d682bf995d44b48d6899e28ffb2
Nada7m <nadamo123123@gmail.com>
2025-03-21 06:23:23 +0300

    Update HW3.js

--- HW3.js
+++ HW3.js
@@ -2,9 +2,6 @@ function tasbih (){
 var count = parseInt(document.getElementById('num').innerHTML)+1;
 document.getElementById('num').innerHTML = count;
 }
-function Pcolor () {
-document.getElementById('num').style.backgroundColor = "#ffe5cc";
-}
 function resit (){
 document.getElementById('num').innerHTML = 0;
 }

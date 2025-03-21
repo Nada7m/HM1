b0e2e6966a216d66b88f2ca231698e702bbcd91c
shahdalsaedi <shahd.alsaedi29@gmail.com>
2025-03-21 03:44:07 +0300

    Update HW3.js

--- HW3.js
+++ HW3.js
@@ -8,3 +8,17 @@ document.getElementById('num').style.backgroundColor = "#ffe5cc";
 function resit (){
 document.getElementById('num').innerHTML = 0;
 }
+const duaa = [ "اللهم بلغنا رمضان", "اللهم انك عفو تحب العفو فاعفو عنا", 
+ "اللهم اغفر لنا وتُب علينا",
+ "اللهم ارزقني حبك وحب كل عملٍ يقربني إليك"];
+ function nextDuaa() {
+	 duaa.push(duaa[0]);
+document.getElementById("duaad").innerHTML = duaa.shift();	
+ }
+ const idea = [ "صدقة سقيا الماء", "ختم القرآن الكريم", 
+ "التبرع بوجبات الإفطار",
+ "صلة الرّحم والإحسان للناس"];
+ function nextidea() {
+	 idea.push(idea[0]);
+document.getElementById("idea").innerHTML = idea.shift();	
+ }
